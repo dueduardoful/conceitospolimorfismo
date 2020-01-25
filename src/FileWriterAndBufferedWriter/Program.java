45eee95c0e7b1216af5808e64ac7e759b73e1a17
dueduardoful <dueduardoful@gmail.com>
2020-01-25 18:05:49 -0300

    utilizando bufferwriter e filewriter

---
+++ src/FileWriterAndBufferedWriter/Program.java
@@ -0,0 +1,26 @@
+package FileWriterAndBufferedWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night", "Hello Word!"};
+
+		String path = "c:\\temp\\out.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch(IOException error) {
+			error.printStackTrace();
+		}
+	}
+
+}
